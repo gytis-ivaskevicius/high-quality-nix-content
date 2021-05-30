f2cdcc3587dcd4f21b187db6a6a03abb7b951074
Gytis Ivaskevicius <me@gytis.io>
2021-05-30 21:58:37 +0300

    Cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -9,30 +9,24 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       args = { creators = import ./creators.nix; };
 
-      emoji = import ./emoji args;
-      anime = import ./anime args;
-      memes = import ./memes args;
-
-      mkMarkdown = size: images:
+      mkMarkdown = height: images:
         pkgs.writeText "images.md"
           (concatStringsSep "\n"
-            (map (it: ''<img height="${toString size}" src="${baseNameOf it.image}"/>'') images));
+            (map (it: ''<img height="${toString height}" src="${baseNameOf it.image}"/>'') images));
+    in {
 
-      emojiMd = mkMarkdown 300 emoji;
-      animeMd = mkMarkdown 700 anime;
-      memesMd = mkMarkdown 700 memes;
-    in
-    {
+      packages.x86_64-linux = rec {
+        emojiMd = mkMarkdown 300 (import ./emoji args);
+        animeMd = mkMarkdown 700 (import ./anime args);
+        memesMd = mkMarkdown 700 (import ./memes args);
 
-      images = { inherit emoji anime; };
-
-      packages.x86_64-linux.build-markdown = pkgs.writeScriptBin "build-markdown" ''
-        cd $(git rev-parse --show-toplevel)
-        cat ${emojiMd} > emoji/README.md
-        cat ${animeMd} > anime/README.md
-        cat ${memesMd} > memes/README.md
-      '';
-
+        build-markdown = pkgs.writeScriptBin "build-markdown" ''
+          cd $(git rev-parse --show-toplevel)
+          cat ${emojiMd} > emoji/README.md
+          cat ${animeMd} > anime/README.md
+          cat ${memesMd} > memes/README.md
+        '';
+      };
 
     };
 }

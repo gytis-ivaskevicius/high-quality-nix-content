3c475ecba219e986b9917acdf3206c8214a9ec34
Gytis Ivaskevicius <me@gytis.io>
2021-05-30 21:54:54 +0300

    Fix one of the memes definitions

--- memes/README.md
+++ memes/README.md
@@ -1,5 +1,5 @@
-<img height="700" src="pinnacle-of-system-configuration.png"/>
+<img height="700" src="nix-20min-adventure.png"/>
 <img height="700" src="nixenv-vs-nixshell.png"/>
-<img height="700" src="virgin-arch-vs-chad-nixos.png"/>
-<img height="700" src="random-repos.png"/>
 <img height="700" src="pinnacle-of-system-configuration.png"/>
+<img height="700" src="random-repos.png"/>
+<img height="700" src="virgin-arch-vs-chad-nixos.png"/>
--- memes/default.nix
+++ memes/default.nix
@@ -1,10 +1,10 @@
 { creators }:
 
 with creators; [
-  { owner = unrooted; image = ./pinnacle-of-system-configuration.png; }
+  { owner = unrooted; image = ./nix-20min-adventure.png; }
   { owner = unrooted; image = ./nixenv-vs-nixshell.png; }
 
-  { owner = unknown; image = ./virgin-arch-vs-chad-nixos.png; }
-  { owner = unknown; image = ./random-repos.png; }
   { owner = unknown; image = ./pinnacle-of-system-configuration.png; }
+  { owner = unknown; image = ./random-repos.png; }
+  { owner = unknown; image = ./virgin-arch-vs-chad-nixos.png; }
 ]

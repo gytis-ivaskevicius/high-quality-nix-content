174f162cf06a02f7090986e19720487c19f39416
Gytis Ivaskevicius <me@gytis.io>
2021-06-24 15:40:12 +0300

    Add nix-glow wallpapers

--- wallpapers/README.md
+++ wallpapers/README.md
@@ -1,1 +1,7 @@
 <img height="700" src="mio-purer-than-your-waifu.png" alt="By unknown author"/>
+
+<img height="700" src="nix-glow.png" alt="By cirkku"/>
+
+<img height="700" src="nix-glow-black.png" alt="By cirkku"/>
+
+<img height="700" src="nix-glow-gruvbox.png" alt="By cirkku"/>
--- wallpapers/default.nix
+++ wallpapers/default.nix
@@ -2,4 +2,8 @@
 
 with authors; [
   { author = unknown; image = ./mio-purer-than-your-waifu.png; }
+
+  { author = cirkku; source = ./sources/nix-glow.blend; image = ./nix-glow.png; }
+  { author = cirkku; source = ./sources/nix-glow.blend; image = ./nix-glow-black.png; }
+  { author = cirkku; source = ./sources/nix-glow.blend; image = ./nix-glow-gruvbox.png; }
 ]

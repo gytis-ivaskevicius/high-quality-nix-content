81e3c00f226eaab02a81aa4debec301e40803bcf
Morgan Jones <me@numin.it>
2024-06-02 15:12:23 -0700

    music: init with "Music to Derive From"

---
+++ music/README.md
@@ -0,0 +1,1 @@
+- [Music to Derive From](https://suno.com/playlist/a5ad16a5-7f4f-480e-9ce3-18059570d5af)
---
+++ music/default.nix
@@ -0,0 +1,5 @@
+{ authors }:
+
+with authors; [
+  { author = numinit; link = "https://suno.com/playlist/a5ad16a5-7f4f-480e-9ce3-18059570d5af"; title = "Music to Derive From"; }
+]

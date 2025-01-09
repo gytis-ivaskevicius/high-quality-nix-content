81039dd1a6f48aaf918f533353b1440d333d2f84
somokill <somokill@noreply.codeberg.org>
2025-01-09 14:37:14 +0500

    Add two anime pictures

--- authors.nix
+++ authors.nix
@@ -116,6 +116,13 @@
     name = "Morgan Jones";
   };
 
+  witttccchhher = {
+    email = "somokill650@gmail.com";
+    github = "witttccchhher";
+    githubId = 192795928;
+    name = "Gregory Mihalkin";
+  };
+
   # Temp
   unknown = {
     name = "unknown author";
